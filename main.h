8100c8310a262a2feba81594bd5e9eacaef1351a
Richard Msule <msuler@yahoo.com>
2022-10-15 17:35:26 -0700

    man page creation

--- main.h
+++ main.h
@@ -1,50 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
-/* utils.c */
-int _strlen(const char *);
-int print(char *);
-char *itoa(long int, int);
-
-/* printf.c */
-int _printf(const char *, ...);
-
-/* handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-
-/* printers */
-int print_string(va_list);
-int print_char(va_list);
-int print_integer(va_list);
-int print_binary(va_list);
-int print_rot(va_list);
-int print_unsigned(va_list);
-int print_octal(va_list);
-int print_hexadecimal_low(va_list);
-int print_hexadecimal_upp(va_list);
-int print_pointer(va_list);
-int print_rev_string(va_list);
-
-/* _putchar.c */
-int _putchar(char);
-int buffer(char);
+#include <string.h>
+#include <unistd.h>
 
 /**
- * struct _format - Typedef struct
- *
- * @type: Format
- * @f: The function associated
- **/
-typedef struct _format
+ * struct types - specifier structure for printf
+ * @p: pointer to characteres specifiers
+ * @func : function pointer to print fucntions
+ */
+typedef struct types
 {
-	char type;
-	int (*f)(va_list);
-} format;
+	char p;
+	int (*func)(va_list);
+} print_f;
 
+int _putchar(char c);
+int _printf(const char *format, ...);
+int printc(va_list l);
+int print_string(va_list s);
+int (*cmp_func(const char a))(va_list);
+int print_n(va_list n);
 
-#endif
+#endif /* Main_h */
